8d4b5f266cc4e6c294addf995790ae8037b25d01
wikearvianti <wikearvianti.dwiputri24@gmail.com>
2021-11-14 18:50:03 +0700

    soal-02

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import MovieCard from "./MovieCard";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieList: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.fetchMovieList(), 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.movieList.length !== this.state.movieList.length) {
+      this.setState({
+        isLoading: false
+      });
+    }
+  }
+
+  async fetchMovieList() {
+    const response = await fetch("https://api.jikan.moe/v3/top/anime");
+    const result = await response.json();
+    this.setState({
+      movieList: result.top || []
+    });
+  }
+
+  render() {
+    return (
+      <div className="container my-5">
+        {/* <h1 className="text-center">{this.state.isLoading ? "Sedang loading" : "Berhasil"}</h1> */}
+        <h1 className="text-center">Anime List</h1>
+        <div className="container my-5">
+          <div id="daftar-anime" className="row">
+            {this.state.isLoading ? (
+              <h1>Loading...</h1>
+            ) : (
+              this.state.movieList.map((movie) => (
+                <MovieCard key={movie.mal_id} movie={movie} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
